6d0f81f177682acef108fae0eebb452cef3fefcd
Mitch Dempsey <mrdempsey@gmail.com>
2011-09-23 17:15:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Antiseptic ##
+
+This application is a PHI scrubber for various medical message formats. It will try to clean any potential PHI out of any message you upload. This message can then be used for testing, or for posting to support forums.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antiseptic::Application.config.secret_token = 'e835a0d478d9242f6d2af519a17f37e55827c7c7fd10f09a0e61496812fc409ac8af105ea2273e9c0086f94e701ba241ed7c7b6bafd26bcf8821c953dcb60807'
